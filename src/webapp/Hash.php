f958f7e0145d2ccd4883080bc6ecf6b6dd6246a7
Markus Andresen <markua@stud.ntnu.no>
2016-10-24 15:29:26 +0200

    hash function changed to blowfish, salt is now a random generated number and cost is set to 14

--- src/webapp/Hash.php
+++ src/webapp/Hash.php
@@ -4,25 +4,34 @@ namespace tdt4237\webapp;
 
 use Symfony\Component\Config\Definition\Exception\Exception;
 
-class Hash
-{
+class Hash{
 
-    static $salt = "password";
+  // blowfish
+    private static $algo = '$2a';
+    // cost parameter
+    private static $cost = '$14';
 
+    public function __construct(){}
 
-    public function __construct()
-    {
+      // mainly for internal use
+      public static function unique_salt() {
+        return substr(sha1(mt_rand()),0,22);
+      }
+
+    // this will be used to generate a hash
+    public static function make($password) {
+
+        return crypt($password,
+                    self::$algo .
+                    self::$cost .
+                    '$' . self::unique_salt());
     }
 
-    public static function make($plaintext)
-    {
-        return hash('sha1', $plaintext . Hash::$salt);
-
-    }
-
-    public function check($plaintext, $hash)
-    {
-        return $this->make($plaintext) === $hash;
-    }
+  // this will be used to compare a password against a hash
+  public function check($password, $hash) {
+      $full_salt = substr($hash, 0, 29);
+      $new_hash = crypt($password, $full_salt);
+      return ($hash == $new_hash);
+  }
 
 }
