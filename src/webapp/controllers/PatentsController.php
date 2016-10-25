cb9393a4d711179f8ff1dce1d42313bc26995b68
Markus Andresen <markua@stud.ntnu.no>
2016-10-25 12:50:23 +0200

    cross site scripting xss prevention implemented

--- src/webapp/controllers/PatentsController.php
+++ src/webapp/controllers/PatentsController.php
@@ -66,7 +66,7 @@ class PatentsController extends Controller
     }
 
     //xss mitigation functions
-  public static function xssafe($data,$encoding='UTF-8'){
+  public function xssafe($data,$encoding='UTF-8'){
    return htmlspecialchars($data,ENT_QUOTES | ENT_HTML401,$encoding);
  }
 
