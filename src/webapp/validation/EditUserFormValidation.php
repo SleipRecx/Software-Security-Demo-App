6529c274bc99af254eea0dd9e512f692dc37953c
corneliugdahling <corneliusdahling@gmail.com>
2016-10-26 20:34:47 +0200

    Limit input length to 50 characters

--- src/webapp/validation/EditUserFormValidation.php
+++ src/webapp/validation/EditUserFormValidation.php
@@ -5,48 +5,60 @@ namespace tdt4237\webapp\validation;
 class EditUserFormValidation
 {
     private $validationErrors = [];
-    
-    public function __construct($email, $phone, $company)
-    {
-        $this->validate($email, $phone, $company);
+
+
+    public function __construct($first_name, $last_name, $email, $phone, $company){
+      $this->validate($first_name, $last_name, $email, $phone, $company);
     }
-    
+
     public function isGoodToGo()
     {
         return \count($this->validationErrors) === 0;
     }
-    
+
     public function getValidationErrors()
     {
         return $this->validationErrors;
     }
 
-    private function validate($email, $phone, $company)
+    private function validate($first_name, $last_name, $email, $phone, $company)
     {
-        $this->validateEmail($email);
-        $this->validatePhone($phone);
-        $this->validateCompany($company);
-    }
-    
-    private function validateEmail($email)
-    {
-        if (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $this->validationErrors[] = "Invalid email format on email";
-        }
-    }
-    
-    private function validatePhone($phone)
-    {
-        if (! is_numeric($phone) or $phone < 00000000 or $phone > 99999999) {
-            $this->validationErrors[] = 'Phoe must be between 00000000 and 99999999.';
-        }
-    }
+      if ($this->stringTooLong($first_name, $last_name, $email, $phone, $company)){
+        $this->validationErrors[] = 'Input field can not have more than 50 characters';
+      }
 
-    private function validateCompany($company)
-    {
-        if(strlen($company) > 0 && (!preg_match('/[^0-9]/',$company)))
-        {
-            $this->validationErrors[] = 'Company can only contain letters';
+      else{
+
+          if(empty($first_name)) {
+              $this->validationErrors[] = "Please write in your first name";
+          }
+
+           if(empty($last_name)) {
+              $this->validationErrors[] = "Please write in your last name";
+          }
+
+          if (strlen($phone) != 8) {
+              $this->validationErrors[] = "Phone number must be exactly eight digits";
+          }
+
+          if (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
+              $this->validationErrors[] = "Invalid email format on email";
+          }
+
+          if(strlen($company) > 0 && (!preg_match('/[^0-9]/',$company)))
+          {
+              $this->validationErrors[] = 'Company can only contain letters';
+          }
         }
-    }
+      }
+
+        public function stringTooLong($input_field1, $input_field2, $input_field3, $input_field4, $input_field5){
+          $length1 = strlen($input_field1);
+          $length2 = strlen($input_field2);
+          $length3 = strlen($input_field3);
+          $length4 = strlen($input_field4);
+          $length5 = strlen($input_field5);
+
+          return $length1 > 50 || $length2 > 50 || $length3 > 50 || $length4 > 50 || $length5 > 50;
+        }
 }
