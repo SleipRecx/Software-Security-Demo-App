f89d1858ea6be078df7f9a787eefbfab630c5df1
corneliugdahling <corneliusdahling@gmail.com>
2016-10-26 18:31:37 +0200

    Limit input field of each registration field

--- src/webapp/validation/RegistrationFormValidation.php
+++ src/webapp/validation/RegistrationFormValidation.php
@@ -27,38 +27,56 @@ class RegistrationFormValidation
 
     private function validate($username, $password, $first_name, $last_name, $phone, $company)
     {
-        if (empty($password)) {
-            $this->validationErrors[] = 'Password cannot be empty';
-        }
+      if ($this->stringTooLong($username, $password, $first_name, $last_name, $phone, $company)){
+        $this->validationErrors[] = 'Input field can not have more than 10 characters';
+      }
 
-        if (!preg_match('/^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/', $password))
-        {
-          $this->validationErrors[] = "Password must contain numbers, capital letters, and be atleast 8 characters long";
-        }
+      else{
 
-        if(empty($first_name)) {
-            $this->validationErrors[] = "Please write in your first name";
-        }
+          if (empty($password)) {
+              $this->validationErrors[] = 'Password cannot be empty';
+          }
 
-         if(empty($last_name)) {
-            $this->validationErrors[] = "Please write in your last name";
-        }
+          if (!preg_match('/^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/', $password))
+          {
+            $this->validationErrors[] = "Password must contain numbers, capital letters, and be atleast 8 characters long";
+          }
 
-        if(empty($phone)) {
-            $this->validationErrors[] = "Please write in your post code";
-        }
+          if(empty($first_name)) {
+              $this->validationErrors[] = "Please write in your first name";
+          }
 
-        if (strlen($phone) != 8) {
-            $this->validationErrors[] = "Phone number must be exactly eight digits";
-        }
+           if(empty($last_name)) {
+              $this->validationErrors[] = "Please write in your last name";
+          }
 
-        if(strlen($company) > 0 && (!preg_match('/[^0-9]/',$company)))
-        {
-            $this->validationErrors[] = 'Company can only contain letters';
-        }
+          if(empty($phone)) {
+              $this->validationErrors[] = "Please write in your post code";
+          }
 
-        if (preg_match('/^[A-Za-z0-9_]+$/', $username) === 0) {
-            $this->validationErrors[] = 'Username can only contain letters and numbers';
+          if (strlen($phone) != 8) {
+              $this->validationErrors[] = "Phone number must be exactly eight digits";
+          }
+
+          if(strlen($company) > 0 && (!preg_match('/[^0-9]/',$company)))
+          {
+              $this->validationErrors[] = 'Company can only contain letters';
+          }
+
+          if (preg_match('/^[A-Za-z0-9_]+$/', $username) === 0) {
+              $this->validationErrors[] = 'Username can only contain letters and numbers';
+          }
         }
+    }
+
+    public function stringTooLong($input_field1, $input_field2, $input_field3, $input_field4, $input_field5, $input_field6){
+      $length1 = strlen($input_field1);
+      $length2 = strlen($input_field2);
+      $length3 = strlen($input_field3);
+      $length4 = strlen($input_field4);
+      $length5 = strlen($input_field5);
+      $length6 = strlen($input_field6);
+
+      return $length1 > 10 || $length2 > 10 || $length3 > 10 || $length4 > 10 || $length5 > 10 || $length6 > 10;
     }
 }
